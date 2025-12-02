f7288bd018a80f9203ea5e7531c9b5748b941674
Wylliam Marinho <marinhowyllian157@gmail.com>
2025-12-02 16:59:57 -0300

    Pré-correção final

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
-# app/main.py
-
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware  # 1. IMPORTE O CORS
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import auth
 
 app = FastAPI(
@@ -10,20 +8,17 @@ app = FastAPI(
     version="1.0.0"
 )
 
-# 2. DEFINA AS ORIGENS PERMITIDAS
-# A URL do seu frontend React
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
-# 3. ADICIONE O MIDDLEWARE DE CORS À SUA APLICAÇÃO
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Permite que essas origens façam requisições
     allow_credentials=True,
-    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
-    allow_headers=["*"],  # Permite todos os cabeçalhos
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
